99def2416c46a9f0e12fd98d911e2ee1a8b20200
Acruxu <giuliodori.juancruz03@gmail.com>
2022-11-10 11:55:04 -0300

    cambiaso

---
+++ Trabajo integrador/Assets/Js/Funcion.js
@@ -0,0 +1,22 @@
+document.getElementById("modificar").onclick=function(){
+document.getElementById("receptor").onclick=function(){
+document.getElementById("cambio").setAttribute("src","assets/Imagenes/peda8.png");
+document.getElementById("cambio").style.display = "block";
+}
+}
+
+document.getElementById("modificar").getAttribute("src",)
+
+var numero
+//creacion de la funcion validar. Esta otorga el numero de la posición apretada.
+function validar(n1){
+    numero= n1
+    return(numero)
+}
+
+console.log(numero);
+
+for(i=0;i<=24;i++){
+document.write((Math.floor(Math.random()*(24-1+1)+1)));
+
+}
